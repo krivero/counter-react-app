d398f3435a3210e198931f5aa5c04f47e924ed4d
Kristina Rivero <kristinarivero@gmail.com>
2018-09-22 12:44:21 -0400

    and add to state and render them

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,33 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
