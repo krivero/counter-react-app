132134103380be53d6604d6cd7189c59c0bbe761
Kristina Rivero <kristinarivero@gmail.com>
2018-09-23 14:17:26 -0400

    rename count state to value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value
+    value: this.props.value
   };
 
   // used arrow function to avoid binding
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
-    console.log("props", this.props);
-
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -28,13 +26,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
