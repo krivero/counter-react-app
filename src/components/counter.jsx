db8a72d94f337a8ec7482524cea92eb25bfb15f6
Kristina Rivero <kristinarivero@gmail.com>
2018-09-23 13:39:22 -0400

    make each counter haveits own div

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,21 +5,23 @@ class Counter extends Component {
     count: 0
   };
 
-  handleIncrement = () => {
+  // used arrow function to avoid binding
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
 
