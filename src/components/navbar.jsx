6e6c85e0a3450a7fc7c98786cba6e9a01e2fe1bd
Kristina Rivero <kristinarivero@gmail.com>
2018-09-24 11:39:10 -0400

    pass props as argument to navbar stateless component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-const NavBar = () => {
+const NavBar = props => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {this.props.totalCounters}
+          {props.totalCounters}
         </span>
       </a>
     </nav>
