b18b75baaf007b0f6519ffbfbf72e0bc3dfc2d41
Kristina Rivero <kristinarivero@gmail.com>
2018-09-24 12:20:00 -0400

    remove link from navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
